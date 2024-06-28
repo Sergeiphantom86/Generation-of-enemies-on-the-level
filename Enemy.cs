1d9259621e51bc2788b18308d1f89eb8255ae0ef
Sergeiphantom86 <96051914+Sergeiphantom86@users.noreply.github.com>
2024-06-28 19:26:23 +0300

    Update Enemy.cs

--- Enemy.cs
+++ Enemy.cs
@@ -1,65 +1,17 @@
-using System;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    private GameObject _target;
-
-    private float _speedMovement = 0.05f;
-    private float _growthRates = 0.1f;
-    private int _targetIndex;
-
-    public static Action<Enemy> _onEnemyMove;
-
-    private void Awake()
-    {
-        _targetIndex = 0;
-        _target = new();
-    }
+    private Vector3 _target;
+    private float _speed = 0.1f;
 
     private void FixedUpdate()
     {
-        IncreaseSize();
-        MovePosition();
+        transform.position = Vector3.MoveTowards(transform.position, _target + Vector3.forward, _speed);
     }
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.collider.GetComponent<Target>())
-        {
-            _onEnemyMove?.Invoke(this);
-        }
-    }
-
-    public void IncreaseSize()
-    {
-        if (transform.localScale.x <= 1)
-        {
-            transform.localScale += transform.lossyScale * _growthRates;
-        }
-    }
-
-    private void MovePosition()
-    {
-        if (transform.localScale.x >= 1)
-        {
-            transform.LookAt(_target.transform.position);
-            transform.position += transform.forward * _speedMovement;
-        }
-    }
-
-    public void SetPointInterest(GameObject target)
+    public void SetTargetPosition(Vector3 target)
     {
         _target = target;
-    }
-
-    public int SetTargetIndex()
-    {
-        return _targetIndex++;
-    }
-
-    public int GetTargetIndex()
-    {
-        return _targetIndex;
     }
 }
